e9b17496f3ac89b967796110c0b7fffb63ad2d8a
masonrware <86129546+masonrware@users.noreply.github.com>
2022-01-25 19:36:58 -0800

    added functionality for updating and deleting data

--- src/ui/app.js
+++ src/ui/app.js
@@ -13,8 +13,8 @@ const insertData = (newData) => {
         body:JSON.stringify(newData)
     })
     .then(resp => resp.json())
-    .then((data) => {
-        console.log(data)
+    .then(() => {
+        getAllData();
     })
     .catch(error => console.log(error))
 }
@@ -29,6 +29,35 @@ const getAllData = () => {
     .then(resp => resp.json())
     .then(data => renderArticles(data))
     .catch(error => console.log(error))
+}
+
+const deleteData = (id) => {
+    fetch(`http://localhost:5000/delete/${id}/`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type':'application/json'
+        }
+    })
+
+    getAllData();
+}
+
+const getDataById = (id) => {
+    fetch(`http://localhost:5000/get/${id}/`, {
+        method:'GET',  
+        headers: {
+            'Content-Type':'application/json'
+        }      
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data['title'])
+    })
+}
+
+const renderOneItem = (mydata) => {
+    title.value = mydata.title,
+    body.value = mydata
 }
 
 function renderArticles(mydata) {
@@ -50,8 +79,8 @@ function renderArticles(mydata) {
             <div class="card-footer text-muted">
                 REPLACE WITH TIME DIF
             </div>
-            <button type="button" class="btn btn-outline-primary" class="btn btn-primary">UPDATE TASK</button>
-            <button type="button" class="btn btn-outline-dark" class="btn btn-primary">DELETE TASK</button>
+            <button type="button" class="btn btn-outline-primary" class="btn btn-primary" style='width:20%' onclick="getDataById(${data.id})">UPDATE TASK</button>
+            <button type="button" class="btn btn-outline-dark" class="btn btn-primary" style='width:20%' onclick="deleteData(${data.id})">DELETE TASK</button>
         </div>
         `
     })
@@ -66,6 +95,7 @@ myform.addEventListener('submit', (e) => {
             body:body.value
         }
         insertData(newData);
+        myform.reset();
     } else {
         alert('Please enter a title or description')
     }
