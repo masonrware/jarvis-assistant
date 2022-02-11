fc8dc2b5d2726c0b8c63879fc6dae678d7180ad3
masonrware <86129546+masonrware@users.noreply.github.com>
2022-02-11 00:12:28 -0500

    set up future plan and removed junk

--- src/ui/classes.js
+++ src/ui/classes.js
@@ -1,5 +1,3 @@
-const { stat } = require("original-fs")
-
 const myform = document.getElementById('taskForm')
 const className = document.getElementById('className')
 const classDays = document.getElementById('classDays')
@@ -62,80 +60,41 @@ function renderCalendar(mydata){
     mydata.forEach(data => {
         name_ = mydata.name
         days_ = mydata.days
-        startTime_ = mydata.start_time
-        endTime_ = mydata.end_time
+        parsedStartTime = parseInt(data.start_time.replace(':', ''));
+        parsedEndTime = parseInt(data.end_time.replace(':', ''));
 
-        //formula for number of divs needed:
-        //end-start = ?XXX
-        //separate ?XXX into ?X and XX (potential ten thousandths and thousands place vales / tenth and hundreths place values)
-        //total_divs = 2(?X) + {1 if XX > 0}
-        
+        //TODO:
+        //so first calc the literal difference between end and start
+        //smooth both values
+        //if parsedCurTime[:secondHalf] > 00:    
+        //save the difference between it and the nearest 100 up in a variable and copy the parsedCurTime and round it up
+        //if parsedEndTime[:secondHalf] > 00:
+        //save the difference between it and the nearest 100 down in a separate varaiable and copy the parsedEndTimen and round it down
 
-        time_diff = endTime_ - startTime_
-        var digits = time_diff.toString().split('');
-        var realDigits = digits.map(Number)
-        
-        time_diff = endTime_ - startTime_
-        var digits = time_diff.toString().split('');
-        var realDigits = digits.map(Number)
-
-
-        if (realDigits.length == 3) {
-            var hrs = realDigits.splice(0,1);
-            var mins = realDigits.splice(0,3);
-        } else if (realDigits.length == 4) {
-            var hrs = realDigits.splice(0,2);
-            var mins = realDigits.splice(0,4);
-        }
-
-        var numBoxes = 2*(hrs.join(""));
-        if(mins[0] != 0) {
-            numBoxes += 1;
-        }
-
-        console.log(numBoxes);
-        console.log(startTime);
-
-        //using the starttime, there will be two for each loops: one that maps through the days and one that maps through the numBoxes:
-        //for each day and for each box, increment by 30 on starttime and create a document id from the day and time.
-        days.forEach(day => {
-            var parsedCurTime = parseInt(startTime.replace(':', ''));
-            var parsedEndTime = parseInt(endTime.replace(':', ''));
-
+        //use the below to get that smoothed space defined
+        days_.forEach(day => {
+            parsedCurTime = parseInt(data.start_time.replace(':', ''));
             while(parsedCurTime!=parsedEndTime){
-                var res = `${day}${parsedCurTime}`;  
-                console.log(res);
-                break;
+                var res = `${day}${parsedCurTime}`;
+                document.getElementById(res).style.backgroundColor = 'red';
+                if (String(parsedCurTime).slice(-2) == 30){
+                    parsedCurTime+=70;
+                } else{
+                    parsedCurTime+=30;   
+                }
             }
-        })
+        })     
+        //once the smoothed space is defined, calculate the before box
+
+        //get the og parsedCurTime and look for the box for the 100 rounded down
+        //figure out what percentage of 100 the minutes difference equates to (out of 60 to 100 formula)
+
+        //replace the prior element with a new element using `` in js that has a height of that percentage
+        //      ** might make the new border clear, so if thats the case either make it black or make all others clear when i switch them
+
+        //repeat for the end time       
     })
 }
-
-// function renderArticles(mydata) {
-//     articles.innerHTML = '';
-//     mydata.forEach(data => {
-//         articles.innerHTML +=  `
-//         <div class="card mb-3">
-//             <h3 class="card-header">${data.name}</h3>
-//             <div class="card-body">
-//                 <h5 class="card-subtitle">${data.start_time} - ${data.end_time}</h5>
-//                 <h6 class="card-subtitle text-muted">TIME LEFT</h6>
-//             </div>
-//             <div class="card-body">
-//                 <p class="card-text">${data.days}</p>
-//             </div>
-//             <div class="card-body">
-//                 <a href="#" class="card-link">OPTIONAL LINK</a>
-//             </div>
-//             <div class="card-footer text-muted">
-//                 REPLACE WITH TIME DIF
-//             </div>
-//             <button type="button" class="btn btn-outline-primary" class="btn btn-primary" style='width:20%' onclick="getTestDataById(${data.id})">UPDATE TASK</button>
-//             <button type="button" class="btn btn-outline-dark" class="btn btn-primary" style='width:20%' onclick="deleteTaskData(${data.id})">DELETE TASK</button>
-//         </div>
-//         `
-//     })
-// }
 
 myform.addEventListener('submit', (e) => {
     if(className.value!=''&&startTime!=''&&endTime!=''&&classDays.value.length!=0){
